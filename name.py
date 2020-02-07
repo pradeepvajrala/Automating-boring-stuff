c114e6efceade4a5bcece73f2359510b00d0cd33
Pradeep Vajrala <pradeepvajralaroyal2001@gmail.com>
2020-02-07 10:39:59 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,10 @@
+#this is a hello world program
+
+print("Me: Hello there!")
+print('You: What is your name?')
+myName=input()
+print('Me: My name is ', myName)
+print('You: Nice to meet you', myName)
+print('You: How old are you,', myName,'?')
+myAge=input()
+print("Me: I will be", str(int(myAge)), 'this year')
